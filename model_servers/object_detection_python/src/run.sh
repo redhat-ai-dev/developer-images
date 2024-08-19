811b911a9c49e01944608614ae8e30ad3c5043b5
Stephanie <yangcao@redhat.com>
2024-08-19 17:10:16 -0400

    remove model-server related. can use ai-lab containerfiles
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- model_servers/object_detection_python/src/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ${MODEL_PATH} ]; then
-    PORT=${PORT} MODEL_PATH=${MODEL_PATH} uvicorn object_detection_server:app --port ${PORT:=8000} --host ${HOST:=0.0.0.0}
-    exit 0
-fi
-
-echo "Please set either a MODEL_PATH"
-exit 1
--- model_servers/whispercpp/src/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./server -tr --model ${MODEL_PATH} --convert --host ${HOST:=0.0.0.0} --port ${PORT:=8001}
